d85333cfd86c0169e97e4b58d85f096433c3a3da
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-11-11 23:59:18 -0500

    Copying template

---
+++ series/002-polar-bar/viz.py
@@ -0,0 +1,18 @@
+import dash
+from dash import html
+from dash import dcc
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("https://raw.githubusercontent.com/curran/data/gh-pages/plotlyExamples/2014_apple_stock.csv")
+fig = px.line(df, x="AAPL_x", y="AAPL_y", width=1200, height=628)
+
+app = dash.Dash()
+app.layout = html.Div(children=[
+  html.H1(children='My Dashboard'),
+  html.Div(children='A practice visualization dashboard.'),
+  dcc.Graph(figure=fig)
+])
+
+if __name__ == '__main__':
+  app.run_server(debug=True)
